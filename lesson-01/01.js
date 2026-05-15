ff4587e50a8177768138f765ece6074ad7d2ff4d
GryachishnikovMihail <gryachishnikov87@gmail.com>
2026-05-15 23:35:11 +0300

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,11 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum=totalSum
+totalSum = 0
+i = 1
+while (i <= 20){
+    totalSum += i
+    print(totalSum) 
+};
